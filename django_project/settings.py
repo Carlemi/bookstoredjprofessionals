4ebea63fe5613f86e2d5467f4bc8e94a3c88e2bf
Carlos <carlos.mendoza007@hotmail.com>
2023-11-20 14:32:37 -0800

    ch-7

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,12 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
 ]
+
+# django-crispy-forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" 
+CRISPY_TEMPLATE_PACK = "bootstrap5" 
 
 # Configuración de autenticación
 AUTHENTICATION_BACKENDS = [
@@ -145,7 +151,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / 'static'] 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
