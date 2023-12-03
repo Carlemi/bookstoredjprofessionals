d87131e6773a928ffbe6e1c757453bb531b854fc
Carlos <carlos.mendoza007@hotmail.com>
2023-12-03 10:38:30 -0800

    'ch13'

--- django_project/settings.py
+++ django_project/settings.py
@@ -193,4 +193,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
